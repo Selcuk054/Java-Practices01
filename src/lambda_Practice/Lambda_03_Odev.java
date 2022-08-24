2b582b4bc7833cda24acc7f4cc27447d52eb4140
Selcuk Ufuk <selcukkrds09@gmail.com>
2022-08-24 21:51:48 +0200

    23.08

--- src/lambda_Practice/Lambda_03_Odev.java
+++ src/lambda_Practice/Lambda_03_Odev.java
@@ -37,4 +37,12 @@ public class Lambda_03_Odev {
     public static double xKuvveti(int sayi, int x) {
         return 0;
     }
+    /*TASK :
+2 Boyutlu bir Multidimensional array i input kabul edip, arraydeki tüm
+elemanları bir
+listeye kopyalayan ve harf sırasına göre yazdıran bir METHOD yazınız.
+Eg : Input : {{Ali,Veli,Ayse},{Hasan,Can},{Suzan}}
+Output:[Ali, Ayse, Can, Hasan, Suzan, Veli]
+*/
+    String[][] arr = {{"Ali", "Veli", "Ayse"}, {"Hasan", "Can"}, {"Suzan"}};
 }
